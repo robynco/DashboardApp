6eced226cfee0d1b5ed0fe40fba5c3463a6b5501
ThresherMaw <code@PALMYRA14>
2016-09-06 20:09:29 +0200

    Test1

--- DashboardApp/Global.asax.cs
+++ DashboardApp/Global.asax.cs
@@ -9,7 +9,9 @@ namespace DashboardApp
   {
     protected void Application_Start()
     {
-      AreaRegistration.RegisterAllAreas();
+            BundleTable.EnableOptimizations = true;
+
+            AreaRegistration.RegisterAllAreas();
 
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
